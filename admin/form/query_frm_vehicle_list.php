49e9e41386738bcd73eea1dfb57a5474abbc31d9
kekengbam <kekengbam10@gmail.com>
2023-07-07 16:30:11 +0530

    upadate _vlist

--- admin/form/query_frm_vehicle_list.php
+++ admin/form/query_frm_vehicle_list.php
@@ -11,14 +11,11 @@ require_once($_SERVER['DOCUMENT_ROOT']. "/demo_copy/path.php");
     $chassis_no=$_POST['chassis_no'];
     $fuel_type=$_POST['fuel_type'];
     
-    if(empty($data)){
-      $stmt = $conn->prepare("INSERT INTO vehicle_list (reg_number, reg_date,manufacture_date,eng_no,chassis_no,fuel_type) VALUES (?,?,?,?,?,?)");
-      $stmt->bind_param("ssssss",$reg_number,$reg_date,$manufacture_date,$eng_no,$chassis_no,$fuel_type);
-      $stmt->execute();
-      $last= $conn->insert_id;
-      echo "Data Inserted";
-    }else{
-      echo "Vehicle Already Exist";
+    $qry="SELECT * FROM vehicle_list ";
+    $result = $db->prepare($qry);
+   $result=$conn->prepare("INSERT INTO 'vehicle_list' (registration_no, reg_date, manufacture_date,eng_no,chassis_no,fuel_type) VALUES (?,?,?,?,?,?)");
+   $result->bind_param("ssssss",$reg_number, $reg_date,$manufacture_date,$eng_no,$chassis_no,$fuel_type);
+   $result-> execute();
+   $last = $conn->insert_id;
     }
-  }
 ?>
